212456d81c0b62a7090c2300f2706c4cd66bda4e
“Mannava123455” <venkatasaimannava9948@gmail.com>
2023-10-17 19:43:57 +0530

    updated

---
+++ GPS/gps_hardware/ham_code/gps.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+#include <memory.h>
+#include <math.h>
+#include <time.h>
+#include"gps.h"
+
+
+
+int main()
+{
+//	int sample_rate=5456000;
+//	int if_freq= 4092000;
+	int sample_rate=2040000;
+	int if_freq= 0;
+	gps_setup(sample_rate,if_freq);
+	//const char *file_name = "gps.bin";
+	const char *file_name = "iq.bin";
+	FILE *fiq;
+	fiq=fopen(file_name,"rb");
+	if(fiq==NULL)
+	{
+		perror("Error inka poyyi paduko");
+		return 1;
+	}
+
+	int c;
+	while((c=getc(fiq)) !=EOF)
+	{
+		for(int i=0;i<8;i++)
+		{
+			if(c&1)
+			{
+				gps_process_sample(1);
+			}
+			else
+			{
+				gps_process_sample(0);
+
+			}
+			  sample_count++;
+			c>>=1;
+		}
+	}
+
+
+/*c = getc(fiq);
+while (c != EOF)
+{
+    int i;
+    for (i = 7; i >= 0; i--)
+    { 
+        if ((c >> i) & 1)
+	{
+            gps_process_sample(1);
+	}
+        else
+	{
+            gps_process_sample(0);
+	}
+        sample_count++;
+    }
+    c = getc(fiq);
+}*/
+
+	  fclose(fiq);
+ 	  printf("%lli samples processed\n",sample_count);
+	return 0;
+}
+
+
