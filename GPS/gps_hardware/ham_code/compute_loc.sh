dcd460bbb1ae02fd1d79e5cbf713d3fbdadebacb
“Mannava123455” <venkatasaimannava9948@gmail.com>
2023-10-20 14:32:50 +0530

    updated

--- GPS/gps_hardware/ham_code/compute_loc.sh
+++ GPS/gps_hardware/ham_code/compute_loc.sh
@@ -1,9 +1,15 @@
+echo "GPS samples are generating at the given location "
 ./gps-sdr-sim -e brdc0010.22n  -s 5456000 -b 1 -d 77 -l 30,120,100
 
+
+echo "GPS samples are generated at 1bit IQ "
 
 gcc -o sample_gen_if sample_gen_if.c
 
 ./sample_gen_if
+
+
+echo "The IF is added to the I+Q samples  "
 
 make
 
