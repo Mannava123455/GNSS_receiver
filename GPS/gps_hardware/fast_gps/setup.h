483ef8cd7c8f6b2fd447c8b239a04c6243e99cc4
“Mannava123455” <venkatasaimannava9948@gmail.com>
2023-10-29 23:24:57 +0530

    fast_gps added

--- GPS/gps_hardware/fast_gps/setup.h
+++ GPS/gps_hardware/fast_gps/setup.h
@@ -1,3 +1,4 @@
+#include <memory.h>
 #define SHOW_TIMING_SNAPSHOT_FREQ_MS     (20)
 
 /* How often should an attempt to solve for position be made */
@@ -368,32 +369,7 @@ struct Snapshot {
     struct Snapshot_entry entries[MAX_SV];
 };
 
-/*********************************************************************************************************/
-/*********************************************************************************************************/
-/************************ THIS CODE IS TAKEN FROM ANOTHER SOURCE *****************************************/
-/*********************************************************************************************************/
-/*********************************************************************************************************/
-//////////////////////////////////////////////////////////////////////////
-// Homemade GPS Receiver
-// Copyright (C) 2013 Andrew Holme
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-// http://www.aholme.co.uk/GPS/Main.htm
-//////////////////////////////////////////////////////////////////////////
 
-#include <memory.h>
 
 #define MAX_ITER 20
 
